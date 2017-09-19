510030eb22237a7f8780fbafa54382b0f377adff
Admin <Admin@DESKTOP-HBC1PKG>
2017-09-19 18:27:06 +0700

    Nat

---
+++ XPath/src/main/java/com/magrabbit/internship/xpath/dao/XPathDAO.java
@@ -0,0 +1,7 @@
+package com.magrabbit.internship.xpath.dao;
+
+import com.magrabbit.internship.xpath.models.XPath;
+
+public interface XPathDAO {
+	public void save(XPath xpath);
+}
---
+++ XPath/src/main/java/com/magrabbit/internship/xpath/service/XPathService.java
@@ -0,0 +1,7 @@
+package com.magrabbit.internship.xpath.service;
+
+import java.util.ArrayList;
+
+public interface XPathService {
+	public ArrayList<String> getXpath(String url);
+}
