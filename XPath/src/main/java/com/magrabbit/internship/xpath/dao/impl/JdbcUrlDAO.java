9c58b9ccfa3408818fe89c5e74bdbb37695ae330
PC-DinhPhan\dinhphan <ngo.loc@heb.com>
2017-09-22 14:12:07 +0700

    abc

--- XPath/src/main/java/com/magrabbit/internship/xpath/dao/impl/JdbcUrlDAO.java
+++ XPath/src/main/java/com/magrabbit/internship/xpath/dao/impl/JdbcUrlDAO.java
@@ -3,6 +3,8 @@ package com.magrabbit.internship.xpath.dao.impl;
 import java.sql.Timestamp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
@@ -15,17 +17,25 @@ public class JdbcUrlDAO implements UrlDAO {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 
+	@Value("${url.save}")
+	private String sqlUrlSave;
+
 	@Override
 	public boolean save(Url url) {
-		this.jdbcTemplate.update("${url.save}",url.getUrl(),url.getDate());
-		return true;
+		boolean rs = false;
+		try {
+			this.jdbcTemplate.update(this.sqlUrlSave, url.getUrl(), url.getDate());
+			rs = true;
+		} catch (DataAccessException e) {
+			System.out.println("ERROR::: save xpath" + e.getMessage()); 
+		}
+		return rs;
 	}
 
 	@Override
 	public int find(String url, Timestamp date) {
 		int id = 0;
-		id = (int)this.jdbcTemplate.queryForObject(
-				"${url.findid}", new Object[] { url,date }, int.class);
+		id = (int) this.jdbcTemplate.queryForObject("${url.findid}", new Object[] { url, date }, int.class);
 
 		return id;
 	}
