07bc14cd85a46a7855fed6c2d1077779221b32aa
Admin <Admin@DESKTOP-HBC1PKG>
2017-09-26 20:00:15 +0700

    connect success with data

--- XPath/src/main/java/com/magrabbit/internship/xpath/dao/impl/JdbcUrlDAO.java
+++ XPath/src/main/java/com/magrabbit/internship/xpath/dao/impl/JdbcUrlDAO.java
@@ -1,7 +1,5 @@
 package com.magrabbit.internship.xpath.dao.impl;
 
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.dao.DataAccessException;
