60800553e3cbfd58ac9355ea48152ed1597df786
Admin <Admin@DESKTOP-HBC1PKG>
2017-10-09 14:55:50 +0700

    nan

--- XPath/src/main/java/com/magrabbit/internship/xpath/controller/XpathController.java
+++ XPath/src/main/java/com/magrabbit/internship/xpath/controller/XpathController.java
@@ -28,10 +28,10 @@ public class XpathController {
 
 	@Autowired
 	XPathService xPathService;
-	
+
 	@Autowired
 	UrlService urlService;
-	
+
 	@Autowired
 	XPathsService xpathsService;
 	static HttpSession session;
@@ -41,78 +41,76 @@ public class XpathController {
 		System.out.println("Home Xpath");
 		return "index";
 	}
-	
+
 	@ResponseBody
 	@RequestMapping(value = "/save", method = RequestMethod.GET)
 	public String save(HttpServletRequest request) {
-		return this.xpathsService.insertXPathsDatabase((XPaths)session.getAttribute("x"));
+		return this.xpathsService.insertXPathsDatabase((XPaths) session.getAttribute("x"));
 	}
-	
-	//@RequestMapping(value = "/getxpath/{url:.*}")
-//	@RequestMapping(value = "/getxpath/{url:regular_expression}")
-//	public XPaths getXpath(@PathVariable("url") String urlget,HttpServletRequest request) {
-//		System.out.println(urlget);
-//		Set<XPath> lstxpath = this.xPathService.getXpath(urlget);
-//		Url url = new Url(urlget);
-//		XPaths xpaths = new XPaths(url,lstxpath);
-//		session = request.getSession();
-//		session.setAttribute("x", xpaths);
-//		return xpaths;
-//	}
-	
+
+	// @RequestMapping(value = "/getxpath/{url:.*}")
+	// @RequestMapping(value = "/getxpath/{url:regular_expression}")
+	// public XPaths getXpath(@PathVariable("url") String urlget,HttpServletRequest
+	// request) {
+	// System.out.println(urlget);
+	// Set<XPath> lstxpath = this.xPathService.getXpath(urlget);
+	// Url url = new Url(urlget);
+	// XPaths xpaths = new XPaths(url,lstxpath);
+	// session = request.getSession();
+	// session.setAttribute("x", xpaths);
+	// return xpaths;
+	// }
+
 	@RequestMapping(value = "/getxpath/{urlBase}/**", method = RequestMethod.GET)
 	@ResponseBody
 	public XPaths getXpath(@PathVariable String urlBase, HttpServletRequest request) {
-	    final String path =
-	            request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE).toString();
-	    final String bestMatchingPattern =
-	            request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE).toString();
+		final String path = request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE).toString();
+		final String bestMatchingPattern = request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE)
+				.toString();
 
-	    String arguments = new AntPathMatcher().extractPathWithinPattern(bestMatchingPattern, path);
+		String arguments = new AntPathMatcher().extractPathWithinPattern(bestMatchingPattern, path);
 
-	    String urlget;
-	    if (null != arguments && !arguments.isEmpty()) {
-	        urlget = urlBase + '/' + arguments;
-	    } else {
-	        urlget = urlBase;
-	    }
-	    urlget = "http://" + urlget;
-	    urlget = urlget.substring(0, urlget.length()-1);
-	    Set<XPath> lstxpath = this.xPathService.getXpath(urlget);
+		String urlget;
+		if (null != arguments && !arguments.isEmpty()) {
+			urlget = urlBase + '/' + arguments;
+		} else {
+			urlget = urlBase;
+		}
+		urlget = "http://" + urlget;
+		urlget = urlget.substring(0, urlget.length() - 1);
+		Set<XPath> lstxpath = this.xPathService.getXpath(urlget);
 		Url url = new Url(urlget);
 		url.setHtml(this.xPathService.getHtml(urlget));
 		System.out.println(this.xPathService.getXpath2(urlget));
-		XPaths xpaths = new XPaths(url,lstxpath);
+		XPaths xpaths = new XPaths(url, lstxpath);
 		session = request.getSession();
 		session.setAttribute("x", xpaths);
 		return xpaths;
 	}
-	
+
 	@RequestMapping(value = "/getxpath2/{urlBase}/**", method = RequestMethod.GET)
 	@ResponseBody
 	public String getXpath2(@PathVariable String urlBase, HttpServletRequest request) {
-	    final String path =
-	            request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE).toString();
-	    final String bestMatchingPattern =
-	            request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE).toString();
+		final String path = request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE).toString();
+		final String bestMatchingPattern = request.getAttribute(HandlerMapping.BEST_MATCHING_PATTERN_ATTRIBUTE)
+				.toString();
 
-	    String arguments = new AntPathMatcher().extractPathWithinPattern(bestMatchingPattern, path);
+		String arguments = new AntPathMatcher().extractPathWithinPattern(bestMatchingPattern, path);
 
-	    String urlget;
-	    if (null != arguments && !arguments.isEmpty()) {
-	        urlget = urlBase + '/' + arguments;
-	    } else {
-	        urlget = urlBase;
-	    }
-	    urlget = "http://" + urlget;
-	    urlget = urlget.substring(0, urlget.length()-1);
-	    Set<XPath> lstxpath = this.xPathService.getXpath(urlget);
+		String urlget;
+		if (null != arguments && !arguments.isEmpty()) {
+			urlget = urlBase + '/' + arguments;
+		} else {
+			urlget = urlBase;
+		}
+		urlget = "http://" + urlget;
+		urlget = urlget.substring(0, urlget.length() - 1);
+		Set<XPath> lstxpath = this.xPathService.getXpath(urlget);
 		Url url = new Url(urlget);
 		url.setHtml(this.xPathService.getHtml(urlget));
-		System.out.println(this.xPathService.getXpath2(urlget));
-		XPaths xpaths = new XPaths(url,lstxpath);
+		XPaths xpaths = new XPaths(url, lstxpath);
 		session = request.getSession();
 		session.setAttribute("x", xpaths);
-		return this.xPathService.getHtml(urlget);
+		return this.xPathService.getXpath2(urlget);
 	}
 }
--- XPath/src/main/java/com/magrabbit/internship/xpath/service/impl/XPathServiceImpl.java
+++ XPath/src/main/java/com/magrabbit/internship/xpath/service/impl/XPathServiceImpl.java
@@ -1,6 +1,6 @@
 package com.magrabbit.internship.xpath.service.impl;
 
-import java.net.URL;
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -33,28 +33,15 @@ public class XPathServiceImpl implements XPathService {
 		return mess;
 	}
 
-	public String getHtml(String url){
-		String html=null;
-		try {
-			Document doc = Jsoup.parse(new URL(url), 10000);
-			String html2 = doc.html();
-			html = html2.replace("<!doctype html>", "");
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return html;
-	}
-	
-	public static Elements getElements(String html) {	
+	public static Elements getElements(String html) {
 		Document doc = Jsoup.parse(html);
 		Elements elements = doc.getAllElements();
 		return elements;
 	}
 
 	public static XPath GenXpathByAttributeId(Element elm) {
-		
-		if(!elm.id().equals("")) {
+
+		if (!elm.id().equals("")) {
 			XPath xpath = new XPath();
 			xpath.setxPath("//*[@id='" + elm.attr("id") + "']");
 			xpath.setElementId(elm.id());
@@ -62,55 +49,60 @@ public class XPathServiceImpl implements XPathService {
 				xpath.setElementName(elm.attr("name"));
 			}
 			return xpath;
-		} else return null;
+		} else
+			return null;
 	}
 
 	public static int checkSiblingIndexLikeTag(Element elm) {
 		Element ep = elm;
 		Element en = elm;
-		int p=0,n=0;
-			while(ep.previousElementSibling()!=null) {
-				if(ep.previousElementSibling().tagName().equals(elm.tagName())) {
-					p=p+1;
-				}
-				ep = ep.previousElementSibling();
+		int p = 0, n = 0;
+		while (ep.previousElementSibling() != null) {
+			if (ep.previousElementSibling().tagName().equals(elm.tagName())) {
+				p = p + 1;
 			}
-			if(p==0) {
-				while(en.nextElementSibling()!=null) {
-					if(en.nextElementSibling().tagName().equals(elm.tagName())) {
-						n=1;break;
-					}
-					en=en.nextElementSibling();
+			ep = ep.previousElementSibling();
+		}
+		if (p == 0) {
+			while (en.nextElementSibling() != null) {
+				if (en.nextElementSibling().tagName().equals(elm.tagName())) {
+					n = 1;
+					break;
 				}
+				en = en.nextElementSibling();
 			}
-			if(p>0) {
-				return p+1;
-			} else if(n==1) {
-				return 1;
-			} else return 0;
+		}
+		if (p > 0) {
+			return p + 1;
+		} else if (n == 1) {
+			return 1;
+		} else
+			return 0;
 	}
-	
+
 	public static XPath GenXpathEleNonAttribute(Element elm) {
 		XPath xpath = new XPath();
 		if (elm.hasAttr("name")) {
 			xpath.setElementName(elm.attr("name"));
 		}
 		StringBuilder path = new StringBuilder("/" + elm.tagName());
-		if(checkSiblingIndexLikeTag(elm)!=0) {
-			path.append("["+checkSiblingIndexLikeTag(elm)+"]");
+		if (checkSiblingIndexLikeTag(elm) != 0) {
+			path.append("[" + checkSiblingIndexLikeTag(elm) + "]");
 		}
 		Elements p_elm = elm.parents();
-		for(Element e : p_elm){		
-			if(GenXpathByAttributeId(e) != null) {
+		for (Element e : p_elm) {
+			if (GenXpathByAttributeId(e) != null) {
 				path.insert(0, GenXpathByAttributeId(e).getxPath());
 				break;
+			} else {
+				if (checkSiblingIndexLikeTag(e) != 0) {
+					path.insert(0, "/" + e.tagName() + "[" + checkSiblingIndexLikeTag(e) + "]");
+				} else
+					path.insert(0, "/" + e.tagName());
 			}
-			if(checkSiblingIndexLikeTag(e)!= 0) {
-				path.insert(0, "/" +e.tagName() + "["+ checkSiblingIndexLikeTag(e) +"]");
-			} else path.insert(0,"/" + e.tagName());
 		}
 		xpath.setxPath(path.toString());
-		
+
 		return xpath;
 	}
 
@@ -123,7 +115,7 @@ public class XPathServiceImpl implements XPathService {
 			Document doc = Jsoup.parse(html);
 			Elements elements = doc.body().getAllElements();
 			for (Element i : elements) {
-				if (GenXpathByAttributeId(i) != null ) {
+				if (GenXpathByAttributeId(i) != null) {
 					lstxPath.add(GenXpathByAttributeId(i));
 				} else
 					lstxPath.add(GenXpathEleNonAttribute(i));
@@ -134,27 +126,41 @@ public class XPathServiceImpl implements XPathService {
 		}
 		return lstxPath;
 	}
-	
+
 	public String getXpath2(String url) {
-		String html2="";
+		String html2 = "";
 		Elements e;
 		try {
 			String html = getHtml(url);
 			Document doc = Jsoup.parse(html);
 			Elements elements = doc.body().getAllElements();
 			for (Element i : elements) {
-				if (GenXpathByAttributeId(i) != null ) {
+				if (GenXpathByAttributeId(i) != null) {
 					i.attr("title", GenXpathByAttributeId(i).getxPath());
 				} else
-				i.attr("title", GenXpathEleNonAttribute(i).getxPath());
+					i.attr("title", GenXpathEleNonAttribute(i).getxPath());
 			}
-			html2 = doc.html();
+			html2 = doc.body().toString();
 		} catch (Exception e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
+
 		return html2;
+	}
+
+	@Override
+	public String getHtml(String url) {
+		String a="";
+		try {
+			Document doc = Jsoup.connect(url).get();
+			a = doc.html();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return a;
 	}
 
 }
